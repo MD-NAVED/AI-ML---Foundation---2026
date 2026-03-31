61d1b3a9e522a05419f46f657dae84b0fc22532f
MD-NAVED <andyk4548@gmail.com>
2026-03-31 23:05:44 +0530

    #Data Trio, Numpy Mathods Prectises

--- Python.py
+++ Python.py
@@ -17,16 +17,16 @@
 # elif(marks <=80 and marks >=70):
 #     grade="C"
 # elif(marks <= 60 and marks >= 50 ):
-#     grade="D"       
-    
+#     grade="D"
+
 # print("GRADE OF THE STUNDENT",grade)
 
 # num = int(input(""))
 # num2 = int(input(""))
 # sum = num+num2
 # print(sum)
- 
-# side = float(input("area of square")) 
+
+# side = float(input("area of square"))
 # # print( side ** 2)
 
 # average = float((input("")))
@@ -40,13 +40,13 @@
 # if(a>= b):
 #     print("true")
 # else:
-#     print("False")    
+#     print("False")
 
 # Num = int(input(""))
 
 # if(Num %2==0 ):
 #     print("Even")
-# else:print("Odd")    
+# else:print("Odd")
 # num = int(input(""))
 # num1 = int(input(""))
 # num2 = int(input(""))
@@ -56,7 +56,7 @@
 # if(num1 >num  and num2):
 #     print(num1)
 # if(num2 > num and num1):
-#     print(num2)    
+#     print(num2)
 
 # Num = int(input(""))
 # if(Num % 7==0):
@@ -71,21 +71,21 @@
 # Grade.sort()
 # print(Grade)
 
-# count =  1 
+# count =  1
 # while count <=5:
 #     print("hy Naved")
 #     count += 1
 
-# Num = 1 
+# Num = 1
 # while Num <=100:
 #     print(Num)
 #     Num+=1
 
-# Num = 100 
+# Num = 100
 # while Num >= 1:
 #     print(Num)
 #     Num-=1
-  
+
 # n= int(input(""))
 # Num = 1
 # while Num <=10:
@@ -112,7 +112,7 @@
 #         print("X found =",les,)
 #         print("At idx =",idx)
 #     idx += 1
-  
+
 # for ren in range(1,101):
     # print(ren)
 
@@ -134,36 +134,36 @@
 # print(sum)
 
 # class Student:
-    
+
 #     def __init__(self,name,marks):
 #        self.name = name
 #        self.marks = marks
-       
+
 #     def welcome(self):
 #      print("student",self.name)
-     
-     
+
+
 #     def Get_marks (self):
 #         print("Marks",self.marks )
-      
-         
+
+
 
 # s1 = Student("karan",97)
 # s1.welcome()
 # s1.Get_marks()
-  
+
 # class  student :
 #     def __init__(self, name, marks):
 #         self.name = name
 #         self.marks = marks
-    
+
 #     def GetAvarage(self):
-#         sum =  0 
-#         for Avg in self.marks:  
+#         sum =  0
+#         for Avg in self.marks:
 #             sum += Avg
-#         print("This is your Avarage:",sum/3)      
-    
-# s1 = student("karan",[56,66,78])    
+#         print("This is your Avarage:",sum/3)
+
+# s1 = student("karan",[56,66,78])
 # s1.GetAvarage()
 
 # class car :
@@ -171,41 +171,41 @@
 #         self.acc = False
 #         self.brk = False
 #         self.clutch = False
-    
+
 #     def start(self):
 #           self.acc = True
 #           self.brk = True
 #           self.clutch = True
 #           print("your car is started")
-         
+
 
 # car1 = car()
 # car1.start()
 
 # class Account:
-    
+
 #      def __init__(self ,bal ,acc):
 #         self.balance = bal
 #         self.accNo  = acc
 #         self.add = [self.balance,self.accNo]
-        
+
 #      def debit(self, amount):
 #          self.balance -= amount
 #          print("RS.",amount, "was debited")
 #          print("Total balance",self.get_balance())
-         
-         
+
+
 #      def cradit(self, amount):
 #          self.balance += amount
 #          print("RS.",amount, "was cardited")
 #          print("Total balance",self.get_balance())
-         
-         
+
+
 #      def get_balance(self):
 #         return self.balance
-         
-       
-        
+
+
+
 # acc1 = Account(10000, 133205145)
 # acc1.debit(1000)
 # acc1.cradit(500)
@@ -216,34 +216,34 @@
 # class car :
 #     def __init__(self,type):
 #         self.type = type
-    
-    
+
+
 #     @staticmethod
 #     def start():
 #      print("Stated..")
-     
+
 #     @staticmethod
 #     def stop():
 #         print("Stoped..")
-        
+
 # class mahindra(car):
 #     def __init__(self, name, type):
 #         super().start()
 #         super().__init__(type)
 #         self.name = name
-        
+
 # car1 = mahindra("thar","electric")
 # print(car1.name,car1.type)
 
-        
+
 # class person:
-#     name = "unknown"  
-    
+#     name = "unknown"
+
 #     # def changeName (self, name):
 #     #     person.name  = name
 #     @classmethod
 #     def changeName (cls, name):
-#         cls.name  = name    
+#         cls.name  = name
 
 # p1 = person()
 # p1.changeName("math")
@@ -251,61 +251,74 @@
 # print(person.name)
 
 
-# @staticmethod #used to create the method which is not related to the class variable and can be called without creating the object of the class
+# @staticmethod #used to create the method which is not related to the class variable
+# and can be called without creating the object of the class
 
-# @classmethod #used to change the class variable without creating the object of the 
+# @classmethod #used to change the class variable without creating the object
+# of the
 
-# @instance #used to change the instance variable and can be      called only by creating the object of the class
-      
-    
+# @instance #used to change the instance variable and can be called only by creating the object
+# of the class
+
+
 # class students:
-    
+
 #     def __init__(self,physics,chemistry,maths):
 #      self.physics = physics
 #      self.chemistry = chemistry
 #      self.maths = maths
-     
+
 #     @property
 #     def average(self):
 #         return str((self.physics + self.chemistry + self.maths)/3) + "%"
-        
-    
+
+
 # stu1 = students(68,90,98)
 # print(stu1.average)
 # stu1.physics =70
 # print(stu1.average)
 
-# @property = used to create the method which is related to the class variable and can be called by creating the object of the class
+# @property = used to create the method which is related to the class variable
+# and can be called by creating the object of the class
 
 
   # {Panding excersise} :-
 
-#polymorphism = the ability of an object to take on many forms. It allows objects of different classes to be treated as objects of a common superclass. In Python, polymorphism is achieved through method overriding and duck typing.
+#polymorphism = the ability of an object to take on many forms. It allows objects of different classes
+# to be treated as objects of a common superclass. In Python, polymorphism is achieved through method overriding and duck typing.
 
-#dunder method = a special method in Python that starts and ends with double underscores (__) and is used to define the behavior of an object for built-in operations. For example, the __str__ method is used to define how an object should be represented as a string when printed.
+#dunder method = a special method in Python that starts and ends with double underscores (__)
+# and is used to define the behavior of an object for built-in operations.
+# For example, the __str__ method
+# is used to define how an object should be represented as a string when printed.
 
 
-# Q1: Define a Circle class to create a circle with radius r using the constructor . Define an Area() method of the class which calculates the area of the circle. Define a Perimeter() method of the class which allows you to calculate the perimeter of the circle.
+# Q1: Define a Circle class to create a circle with radius r using the constructor.
+# Define an Area() method of the class which calculates the area of the circle.
+# Define a Perimeter() method of the class which allows you to calculate the perimeter of the circle.
 
 
 
 # class circle:
 #     def __init__(self,radius):
 #         self.radius = radius
-        
+
 #     def Area(self):
 #         return 3.14 * self.radius **2
-    
+
 #     def perimeter(self):
 #         return 2 * 3.14 * self.radius
-    
+
 # c1 = circle(21)
 # print("Area of the circle is",c1.Area())
 # print("Perimeter of the circle is",c1.perimeter())
 
 
 
-# Q2: Define an Employee class with attributes role, department & salary (0:59:51). This class should have a showDetails() method (1:05:32). Create an Engineer class that inherits properties from Employee & has additional attributes: name & age (1:06:17).
+# Q2: Define an Employee class with attributes role, department & salary
+# (0:59:51).
+# This class should have a showDetails() method (1:05:32).
+# Create an Engineer class that inherits properties from Employee & has additional attributes: name & age (1:06:17).
 
 
 
@@ -314,7 +327,7 @@
 #         self.role = role
 #         self.department = department
 #         self.salary = salary
-       
+
 #     def showDetails(self):
 #         print("role:",self.role)
 #         print("department:",self.department)
@@ -323,13 +336,13 @@
 #         print("age:",self.age)
 
 # class Engineer(Empoyee):
-    
+
 #     def __init__(self, name,age):
 #             super().__init__("software engineer", "IT", 50000)
 #             self.name = name
-#             self.age = age 
-            
-           
+#             self.age = age
+
+
 # e1 = Engineer("naved", 24)
 # e1.showDetails()
 
@@ -337,14 +350,12 @@
 #      def __init__(self, item, prise):
 #          self.prise = prise
 #          self.item = item
-             
+
 #      def __gt__(self, order2):
 #         return self.prise > order2.prise
-    
-    
+
+
 # order1 = Ordera("bergur",100)
 # order2 = Ordera("pizza", 200)
 # print(order1 > order2)
-
-
 
---
+++ numPy.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+  # print(x.shape,x.dtype) #.shape and .dtype are attributes of the numpy array. .shape gives the dimensions of the array, and .dtype gives the data type of the elements in the array.
+
+# x = np.array([[1, 2, 3,4 ],
+#               [4, 5, 6,7 ],
+#               [7, 8, 9,10]])                            
+
+# x = np.array([[1, 2, 3],
+#               [4, 5, 6],])
+
+# # print(x.shape,x.dtype)
+
+# x.__array__(float)
+# print(x.__array__((float)))
+
+
+ # .ndim is an attribute of the numpy array that gives the number of dimensions of the array. In this case, since  
+#  x is a 2D array (a matrix), x.ndim will return 2.
+
+# x = np.array([[1, 2, 3,4 ],   
+#               [4, 5, 6,7 ],
+#               [7, 8, 9,10]])
+                         
+# print(x.ndim)
+
+
+# .astype() is a method of the numpy array that allowas you to cast the elements of the array to a different data type. In this case, x.astype
+# (float) willreturn a new array with the same shape as x but with all elements converted to the float data type.
+
+
+# x = np.array([[1.2,2.5,3.8,]
+#               ,[4.1,5.6,6.3]
+#               ,[7.4,8.9,9.0]])
+# print(x)
+# print(x.dtype)
+# print(x.astype(int))
+ 
+ 
+ # mathamatical operations on numpay arrays are performed element-wise.This means that when you perfornm an operation on a numpay array, the operation is applied to each elements of the array individually.For example, if you add two numpay arrays togather, the result will be a new array where each elements is the sum of the corresponding elements from the original arrays.
+ 
+ 
+ 
+# arr = np.array([10,20,30])
+# print(arr + 5)
+# print(arr * 2)
+# print((arr / 2))
+# print(arr **2)
+
+#Agrigation functions in numpy are used to perform operation that summarize or reduse the data in an array. This functions include sum(), mean(),min(),max(),std(),and var().This function can be applied to the entire array or along a specific axis. For example,if you have a 2D array,you can use these functions to calculate the sum or mean of each row or each colum by specifying the axis parameter.
+
+
+# agri_arr = np.array([10,20,30,40,50])
+
+# print(np.sum(agri_arr))    #This will calculate the sum of all eliment in the array and return 150.
+# print(np.mean(agri_arr))   #This will calculate the mean of all elements in the array and return 30.0.
+# print(np.min(agri_arr))    #This will calculate the minimum value in the array and return 10.
+# print(np.max(agri_arr))    #This will calculate the maximum value in the array and return 50.
+# print(np.std(agri_arr))    #This will calculate the standard deviation of all elements in the array and return approximately 14.14.
+# print(np.var(agri_arr))    #This will calculate the variance of all elements in the array and return approximately 200.0.
+
