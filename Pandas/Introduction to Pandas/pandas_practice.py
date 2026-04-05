1e5d492a87eb4391a304565f3b294a489fd69752
MD-NAVED <andyk4548@gmail.com>
2026-04-05 13:57:19 +0530

    Accurate File & Folder structure and into... to Pandas

---
+++ Pandas/Introduction to Pandas/pandas_practice.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+df = pd.DataFrame({
+    "Name" : ["Andy", "Bob", "Charlie", "David"],
+    "Age" : [25, 30, 35, 40],
+    "City" : ["New York", "Los Angeles", "Chicago", "Houston"]
+
+})   
+
+# print(df["Age"]) 
+
+# print(df["Name"])
+
+# print(df["City"])
+
