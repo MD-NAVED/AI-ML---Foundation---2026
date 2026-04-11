17d178ca3eedead9d5670b8cecfd5c62f044edb9
MD-NAVED <andyk4548@gmail.com>
2026-04-11 18:50:45 +0530

    feat: add pandas apply and map practice examples
    - Included examples of the .apply() method in conjunction with .groupby() for calculating custom metrics (e.g., profit ratios and weighted averages).
    - Added usage examples of .map() on categorical data using lambda functions.
    - Practiced dictionary-based mapping and explored
    a_action parameters.

--- Pandas/Feature Engineering/pan.py
+++ Pandas/Feature Engineering/pan.py
@@ -43,3 +43,64 @@ import numpy as np
 # print(pd.get_dummies(pd.Series(list("abc")),dtype=float)) # dtype is used to convert the dummy variables into float type.
 
 
+
+# .apply() method is used to apply a function along an axis of the DataFrame.
+
+
+
+# df = pd.DataFrame({
+#     'Branch': ['East', 'East', 'West', 'West'],
+#     'Sales': [100, 150, 200, 50],
+#     'Returns': [5, 10, 20, 5]
+# })
+
+# # def profit_ratio(group):
+#     return group['Sales'].sum() / group['Returns'].sum()
+
+# print(df.groupby('Branch').apply(profit_ratio))
+
+
+# def get_status(group):
+#     return pd.Series({
+#         'Max_Sales': group['Sales'].max(),
+#         'Min_Sales': group['Sales'].min()
+#     })
+
+# status = df.groupby('Branch').apply(get_status)
+# print(status)
+
+
+# df = pd.DataFrame({
+#     'Category': ['A', 'A', 'B', 'B'],
+#     'Value': [10, 20, 30, 40],
+#     'Weight': [1, 2, 1, 2]
+# })
+
+# print(df)
+# weighted_avg = df.groupby('Category').apply(lambda  x: (x['Value']* x['Weight']).sum() / x['Weight'].sum()) 
+# print(weighted_avg)
+
+
+# .map() method is used to map values of a Series using a given dictionary or function.
+
+cat  = pd.Categorical(['a','b','c'])
+
+# print(cat.map(lambda x: x.upper())) # with function
+
+# print(cat.map({'a':1,'b':2,'c':3})) # with dictionary
+
+# print(cat.map({'a': 'first' , 'b': 'second' , 'c': 'third'})) # with index mapping  
+
+# print(cat.map({"a": "first", "b": "second"}, na_action=None)) # with na_action=None
+
+# cat = pd.Categorical(["a", "a", "b"]) 
+# calls = []
+# def f(x):
+#     calls.append(x)
+#     return x.upper()
+# result = cat.map(f) 
+# print(result)
+
+
+
+
