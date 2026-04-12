b5051887c3ae32c3a56e6e6547eb48f12382bca1
MD-NAVED <andyk4548@gmail.com>
2026-04-12 10:42:10 +0530

    feat: add pandas dataset combining examples
    - Created new pan.py script for practicing dataframe combinations.
    - Added examples of concatenating dataframes vertically and horizontally using pd.concat().
    - Practiced SQL-like dataframe merging using pd.merge() based on common key columns (Roll_No) with outer joins.

---
+++ Pandas/Combining Datasets/pan.py
@@ -0,0 +1,49 @@
+# Combining Datasets
+
+# If your features are spread across multiple tables, you need to bring them together.
+
+# *   **`pd.concat()`**: Concatenates DataFrames along a particular axis (stacking them vertically or horizontally).
+
+# *   **`pd.merge()`**: Joins DataFrames based on a common key column (similar to SQL joins).
+
+
+import pandas as pd
+
+
+#concat() :-
+
+df1 = pd.DataFrame({
+    "Name" : ['Anmit','Bhavna'],
+    "Score":['85',92]
+})
+
+df2 = pd.DataFrame({
+    'Name': ['Chetan', 'Diya'],
+    'Score': [78, 88]
+})
+
+# print(df1)
+# print(df2)
+
+combine_df = pd.concat([df1, df2],ignore_index=True)
+combine_df = pd.concat([df1, df2],ignore_index=True,axis=1)
+# print(combine_df)
+
+
+# mearge() :-
+
+df_students = pd.DataFrame({
+    'Roll_No': [1, 2, 3],
+    'Name': ['Amit', 'Bhavna', 'Chetan']
+})
+
+
+df_marks = pd.DataFrame({
+    'Roll_No': [2, 1, 4],
+    'Subject_Marks': [95, 88, 70]
+})
+
+final_result = pd.merge(df_students,df_marks,on='Roll_No',how='outer') # inner, left, right, outer
+print(final_result)
+
+
