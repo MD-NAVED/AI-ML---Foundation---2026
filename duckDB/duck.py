47db5877d10b6e7523db3d79b737a248647818c9
MD-NAVED <andyk4548@gmail.com>
2026-04-18 20:04:10 +0530

    Title: feat: Add In-Memory SQL Data Pipeline with DuckDB Description:
    Configured read_csv_auto to directly query raw datasets without Pandas overhead.
    Implemented complex aggregate SQL logic combining SELECT, WHERE, YEAR(), GROUP BY, and ORDER BY.
    Handled Date to Integer datatype conversion successfully.

--- duckDB/duck.py
+++ duckDB/duck.py
@@ -1,38 +1,96 @@
 from duckdb import query
 import duckdb 
 import pandas as pd
+import  os
+
+# data = {
+#     'Name': ['Amit', 'Rahul', 'Neha', 'Pooja', 'Rohan'],
+#     'Age': [25, 30, 28, 22, 35],
+#     'Salary': [50000, 60000, 55000, 45000, 70000],
+#     'Department': ['IT', 'HR', 'IT', 'Finance', 'HR']
+# }
+
+# df = pd.DataFrame(data)
+
+# query = """
+# SELECT Name ,Salary
+# FROM df
+# """
+
+# query2 = """
+# SELECT * 
+# FROM df
+# WHERE Age > 25
+# """
+
+# query3 = """
+# SELECT Name ,Salary
+# FROM df
+# WHERE Department = 'IT'
+# ORDER BY Salary DESC
+# """
 
 
-data = {
-    'Name': ['Amit', 'Rahul', 'Neha', 'Pooja', 'Rohan'],
-    'Age': [25, 30, 28, 22, 35],
-    'Salary': [50000, 60000, 55000, 45000, 70000],
-    'Department': ['IT', 'HR', 'IT', 'Finance', 'HR']
-}
+# query = """
+#     SELECT 
+#         Department, 
+#         MAX(Age) as Sabse_Bada_Employee_Age, 
+#         SUM(Salary) as Total_Department_Karcha
+#     FROM df
+#     GROUP BY Department
+# """
 
-df = pd.DataFrame(data)
+# result_df = duckdb.query(query).df()
+# print(result_df)
 
-query = """
-SELECT Name ,Salary
-FROM df
+
+file_path = "C:/Users/andyk/Desktop/AI-ML Foundation/data/vds.csv"
+
+
+# d_path = r"C:\Users\andyk\Desktop\AI-ML Foundation\data"
+# print(os.listdir(d_path))
+
+
+
+
+query = f"""
+SELECT *
+FROM '{file_path}'
 """
 
-query2 = """
-SELECT * 
-FROM df
-WHERE Age > 25
+
+query = f"""
+    SELECT 
+        Publisher,
+        SUM(total_sales) as Total_Sales
+    FROM read_csv_auto('{file_path}')
+    GROUP BY Publisher
+    ORDER BY Total_Sales DESC
+    LIMIT 5
 """
 
-query3 = """
-SELECT Name ,Salary
-FROM df
-WHERE Department = 'IT'
-ORDER BY Salary DESC
+
+# query = f"""
+#     SELECT
+#         genre,
+#         COUNT(*) as Total_Action_Games
+#         FROM '{file_path}'
+#         WHERE genre = 'Action'
+#         GROUP BY genre
+#         ORDER BY Total_Action_Games DESC
+#         LIMIT 5
+#         """
+
+
+query = f"""
+    SELECT 
+        title,
+        total_sales
+    FROM read_csv_auto('{file_path}')
+    WHERE YEAR(release_date) = 2010
+    ORDER BY total_sales DESC
+    LIMIT 5
+    
 """
-
-
-
-
 result_df = duckdb.query(query).df()
-print(result_df)
-
+print(result_df)    
