760204d0c6fb4ed9fbad0185e173998942eb7073
MD-NAVED <andyk4548@gmail.com>
2026-04-18 22:22:50 +0530

    feat: Implement high-performance direct CSV querying using DuckDB and SQL

--- duckDB/duck.py
+++ duckDB/duck.py
@@ -47,27 +47,25 @@ import  os
 file_path = "C:/Users/andyk/Desktop/AI-ML Foundation/data/vds.csv"
 
 
-# d_path = r"C:\Users\andyk\Desktop\AI-ML Foundation\data"
-# print(os.listdir(d_path))
 
 
 
 
-query = f"""
-SELECT *
-FROM '{file_path}'
-"""
+# query = f"""
+# SELECT *
+# FROM '{file_path}'
+# """
 
 
-query = f"""
-    SELECT 
-        Publisher,
-        SUM(total_sales) as Total_Sales
-    FROM read_csv_auto('{file_path}')
-    GROUP BY Publisher
-    ORDER BY Total_Sales DESC
-    LIMIT 5
-"""
+# query = f"""
+#     SELECT 
+#         Publisher,
+#         SUM(total_sales) as Total_Sales
+#     FROM read_csv_auto('{file_path}')
+#     GROUP BY Publisher
+#     ORDER BY Total_Sales DESC
+#     LIMIT 5
+# """
 
 
 # query = f"""
@@ -82,15 +80,73 @@ query = f"""
 #         """
 
 
+# query = f"""
+#     SELECT 
+#         title,
+#         total_sales
+#     FROM read_csv_auto('{file_path}')
+#     WHERE YEAR(release_date) = 2010
+#     ORDER BY total_sales DESC
+#     LIMIT 5
+    
+# """
+
+
+# query = f"""
+    
+#     SELECT 
+#         console,
+#         SUM(total_sales) as Duniya_Bhar_Ki_Income
+#     FROM read_csv_auto('{file_path}')
+#     GROUP BY console
+#     ORDER BY Duniya_Bhar_Ki_Income DESC
+#     LIMIT 5
+
+
+# """
+
+# ==========================================
+# 💼 TASK 3: Double Filter ka Jadoo (AND)
+# ==========================================
+# Manager ki demand: "Naved, 'Action' games toh bohot saare publishers banate hain, par mujhe un
+# Action games ki list chahiye jo EXCLUSIVELY 'Nintendo' company ne banaye hon. Mujhe 
+# un Games ka Title aur unki Total Sales chahiye."
+# 
+# Aapke liye Hints:
+# 1. Dikhana kya kya hai: SELECT title, total_sales
+# 2. Dataset lana hai: FROM read_csv_auto('{file_path}')
+# 3. Yahan 'Double Filter' lagega: jahan genre 'Action' ho AUR publisher 'Nintendo' ho.
+#    (Aise Likhna Hai -> WHERE genre = 'Action' AND publisher = 'Nintendo')
+# 4. Group (SUM/COUNT) karne ki koi zaroorat nahi hai.
+# 5. Rank wise highest pehle: ORDER BY total_sales DESC
+# 6. Sirf Top 5 Theek rahenge: LIMIT 5
+# ==========================================
+
 query = f"""
-    SELECT 
+     
+      SELECT 
         title,
         total_sales
     FROM read_csv_auto('{file_path}')
-    WHERE YEAR(release_date) = 2010
+    WHERE genre = 'Action' AND publisher = 'Nintendo'
     ORDER BY total_sales DESC
     LIMIT 5
-    
+
+
 """
+
+
+# result_df = duckdb.query(query).df()
+# print("\n--- TOP 5 SELLING PLATFORMS 🎮 ---")
+# print(result_df)
+
+
+
+
 result_df = duckdb.query(query).df()
-print(result_df)    
+print("\n--- TOP 5 SELLING GAMES OF Nintendo 🎮 ---")
+print(result_df)
+
+
+
+
